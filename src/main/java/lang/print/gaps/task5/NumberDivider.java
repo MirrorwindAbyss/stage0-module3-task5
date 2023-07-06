85c01c86f1521969fb2f9ee3cac4d2f138cdd262
Nabiyev Abdumalik <abdumalikunusov06@gmail.com>
2023-07-06 18:54:16 +0500

    Done!

--- src/main/java/lang/print/gaps/task5/NumberDivider.java
+++ src/main/java/lang/print/gaps/task5/NumberDivider.java
@@ -3,7 +3,7 @@ package lang.print.gaps.task5;
 public class NumberDivider {
     void divide(int toBeDivided) {
         double result;
-        result=toBeDivided/5;
+        result=toBeDivided/5.0;
         System.out.println(result);
     }
 }
